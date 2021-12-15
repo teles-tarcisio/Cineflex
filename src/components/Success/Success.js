c31363bdb824324fd62d0d93c4049eca515811b0
Tarcísio Teles <tfst@ecomp.poli.br>
2021-12-15 19:47:00 -0300

    feat: layout 'success' screen

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -1,26 +1,37 @@
-
+import "../MoviesList/MoviesList.css";
+import "./Success.css";
 
 
 export default function Success() {
+  const reservation = {
+    movieTitle: "Project Power",
+    sessionDate: "24/02/2022",
+    sessionTime: "19:00",
+    seats: [14474, 14473, 14475, 14476],
+    name: "João da Silva Sauro",
+    cpf: "22464246409"
+  };
+
   return (
     <>
       <div className="order-summary">
         <div className="movie-session">
           <h2>Filme e sessão</h2>
-          <p>reservation.movieTitle</p>
-          <p>reservation.sessionDate - reservation.sessionTime</p>
+          <p>{reservation.movieTitle}</p>
+          <p>{reservation.sessionDate} - {reservation.sessionTime}</p>
         </div>
 
         <div className="tickets">
           <h2>Ingressos</h2>
-          <p>Assento seat</p>
-          
+          {reservation.seats.map((seat) => (
+            <p>Assento {seat}</p>
+          ))}          
         </div>
       
         <div className="success-buyer-data">
         <h2>Comprador</h2>
-        <p>Nome reservation.nome</p>
-        <p>CPF: reservation.cpf</p>
+        <p>Nome: {reservation.name}</p>
+        <p>CPF: {reservation.cpf}</p>
         </div>
       </div>
 
