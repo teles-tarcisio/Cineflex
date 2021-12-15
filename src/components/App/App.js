ea8fa71a3566fccc436db42dedb119f55e6b4b66
Tarcísio Teles <tfst@ecomp.poli.br>
2021-12-15 20:38:02 -0300

    feat: started adding Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 import MoviesList from "../MoviesList/MoviesList.js";
@@ -5,25 +7,36 @@ import Sessions from "../Sessions/Sessions.js";
 import Seats from "../Seats/Seats.js";
 import Success from "../Success/Success.js";
 
+
+function InfoBox() {
+  return (
+    <>
+      <div className="information-box">
+        <h1>Selecione_filme-horário-assentos/Pedido_Sucesso</h1>
+      </div>
+      <MoviesList />
+    </>
+  );
+}
+
 export default function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <header><h1>CINEFLEX</h1></header>
 
       <div className="main-container">
-        
-        <div className="information-box">
-          <h1>Selecione_filme-horário-assentos/Pedido_Sucesso</h1>
-        </div>
+        <Routes>
+          <Route path="/" element={<InfoBox />}></Route>
+          
+          <Route path="/sessoes/:movieID" element={<Sessions />}></Route>
 
-        {/* <MoviesList /> */}
-        {/* <Sessions /> */}
-        {/* <Seats /> */}
-        <Success />
+          <Route path="/assentos/:sessionID" element={<Seats />}></Route>
 
+          <Route path="/sucesso" element={<Success />}></Route>
 
+        </Routes>
       </div>
-    </>
+    </BrowserRouter >
   );
 }
