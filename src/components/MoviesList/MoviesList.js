034550328662ce0faa31f951992bb3706f18715a
Tarcísio F. S. Teles <tfst@poli.br / tfst@ecomp.poli.br>
2021-12-16 20:33:41 -0300

    clean: removing console.log's

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -8,27 +8,16 @@ const MOVIESLIST_URL = "https://mock-api.driven.com.br/api/v4/cineflex/movies";
 
 export default function MoviesList() {
   const [allMoviesList, setMoviesList] = useState([]);
-  console.log(allMoviesList);
+  
   const getAllMovies = (
     () => {
       const promise = axios.get(MOVIESLIST_URL);
       promise.then( response => {
         setMoviesList([...response.data]);
-        console.log(allMoviesList);
       });
     }
   );
-
   useEffect(getAllMovies, []);
-  /*
-  if (allMoviesList.length === 0) {
-    return (
-      <ul className="movies-list">
-        <h1>Loading movies...</h1>
-      </ul>
-    );
-  }
-  */
   
   return (
     <ul className="movies-list">
